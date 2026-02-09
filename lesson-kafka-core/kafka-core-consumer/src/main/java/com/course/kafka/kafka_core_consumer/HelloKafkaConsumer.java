7579771a4abb014372448def896ede514e654c28
Emmanuel Mwiti <kiuguemmanu@gmail.com>
2026-02-09 11:22:48 +0300

    Project setup done

---
+++ lesson-kafka-core/kafka-core-consumer/src/main/java/com/course/kafka/kafka_core_consumer/HelloKafkaConsumer.java
@@ -0,0 +1,13 @@
+package com.course.kafka.kafka_core_consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloKafkaConsumer {
+
+    @KafkaListener(topics = "t-hello")
+    public void consume(String message) {
+        System.out.println("Consumer from Kafka Consumer" + message);
+    }
+}
---
+++ lesson-kafka-core/kafka-core-producer/src/main/java/com/course/kafka/kafka_core_producer/KafkaCoreProducerApplication.java
@@ -0,0 +1,18 @@
+package com.course.kafka.kafka_core_producer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCoreProducerApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaCoreProducerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+
+}
